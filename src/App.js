62e67c9c75d849a31103a3b371368acf063782f8
aumervejss <72447846+mervejss@users.noreply.github.com>
2025-03-07 22:40:11 +0300

    mervecommit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Loading... DSLKGŞSDG HOŞGELDİN
+          Loading... DSLKGŞSDG HOŞGELDİNhjgkhgkkgk
         </p>
       </header>
     </div>
